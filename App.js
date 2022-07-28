95375e18b8986b81d99cac2a3331a50dc85c8e1c
Luciano Wayand de Abreu <lucianowayand@hotmail.com>
2022-07-28 12:49:09 -0300

    feat: NFC read and write

---
+++ App.js
@@ -0,0 +1,104 @@
+import React from "react";
+import { View, Text, Button, TextInput } from "react-native";
+import NfcManager, { NfcTech } from 'react-native-nfc-manager';
+
+NfcManager.start();
+
+export default function App() {
+  
+  async function writeNdef(){
+    try {
+      let tech = Platform.OS === 'ios' ? NfcTech.MifareIOS : NfcTech.NfcA;
+      let resp = await NfcManager.requestTechnology(tech);
+
+      let text = "asdalsdkaslpdkad";
+      let fullLength = text.length + 7;
+      let payloadLength = text.length + 3;
+
+      let cmd = Platform.OS === 'ios' ? NfcManager.sendMifareCommandIOS : NfcManager.transceive;
+
+      resp = await cmd([0xA2, 0x04, 0x03, fullLength, 0xD1, 0x01]); // 0x0C is the length of the entry with all the fluff (bytes + 7)
+      resp = await cmd([0xA2, 0x05, payloadLength, 0x54, 0x02, 0x65]); // 0x54 = T = Text block, 0x08 = length of string in bytes + 3
+
+      let currentPage = 6;
+      let currentPayload = [0xA2, currentPage, 0x6E];
+
+      for(let i=0; i<text.length; i++){
+          currentPayload.push(text.charCodeAt(i));
+          if (currentPayload.length == 6){
+              resp = await cmd(currentPayload);
+              currentPage += 1;
+              currentPayload = [0xA2, currentPage];
+          }
+      }
+
+      currentPayload.push(254);
+      while(currentPayload.length < 6){
+          currentPayload.push(0);
+      }
+
+      resp = await cmd(currentPayload);
+      console.log(resp)
+
+    } catch(error){
+      console.warn("Oops!", error)
+    }
+  }
+
+  async function readNdef(){
+    try {
+      let tech = Platform.OS === 'ios' ? NfcTech.MifareIOS : NfcTech.NfcA;
+      let resp = await NfcManager.requestTechnology(tech);
+
+      let cmd = Platform.OS === 'ios' ? NfcManager.sendMifareCommandIOS : NfcManager.transceive;
+
+      resp = await cmd([0x3A, 4, 4]);
+      let payloadLength = parseInt(resp.toString().split(",")[1]);
+      let payloadPages = Math.ceil(payloadLength / 4);
+      let startPage = 5;
+      let endPage = startPage + payloadPages - 1;
+
+      resp = await cmd([0x3A, startPage, endPage]);
+      bytes = resp.toString().split(",");
+      let text = "";
+
+      for(let i=0; i<bytes.length; i++){
+          if (i < 5){
+              continue;
+          }
+          if (parseInt(bytes[i]) === 254){
+              break;
+          }
+          text = text + String.fromCharCode(parseInt(bytes[i]));
+      }
+      console.warn(text)
+  } catch (error) {
+    console.warn("Oops!", error)
+  }
+}
+  
+
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <View>
+        <Text style={{padding:20, fontSize:22}}>Write on an NFC TAG</Text>
+        <TextInput 
+          style={{borderColor:'black', borderWidth:1, borderRadius:5}}
+          maxLength={20} 
+        />
+        <View style={{paddingTop:20, display:'flex', alignItems:'center'}}>
+          <View style={{width:225}}>
+            <Button title="Write" onPress={writeNdef}/>
+          </View>
+        </View>
+      </View>
+      <View>
+        <Text style={{padding:20, fontSize:22}}>Scan an NFC TAG</Text>
+        <Button title="Read" onPress={readNdef}/>
+      </View>
+    </View>
+    
+  );
+};
+
+
